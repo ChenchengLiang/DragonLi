3af5c8774a0342a21ad650621edd9e43e3e8cb29
Chencehng Liang <lcckkkhaha@163.com>
2023-11-16 12:06:30 +0100

    refactor

--- src/solver/models/generate_configurations.py
+++ src/solver/models/generate_configurations.py
@@ -22,25 +22,25 @@ def main():
             for gnn_layer_num in [2,4,8]:
                 for ffnn_layer_num in [2]:
                     for hidden_dim in [128]:
-                        for gnn_dropout_rate in [0.5]:
+                        for dropout_rate in [0.5]:
                             for model_type in ["GCN","GIN","GCNwithGAP","MultiGNNs"]:  # ["GCN", "GAT", "GIN","GCNwithGAP","MultiGNNs"]
                                 if model_type == "GAT":
                                     for num_heads in [1]:
                                         configurations.append({
                                             "benchmark": benchmark, "graph_type": graph_type, "model_type": model_type,
                                             "num_epochs": num_epochs, "learning_rate": 0.001,
-                                            "save_criterion": "valid_accuracy", "batch_size": 100, "gnn_hidden_dim": hidden_dim,
-                                            "gnn_layer_num": gnn_layer_num, "num_heads": num_heads, "gnn_dropout_rate": gnn_dropout_rate,
-                                            "ffnn_hidden_dim": hidden_dim, "ffnn_layer_num": ffnn_layer_num, "ffnn_dropout_rate": 0
+                                            "save_criterion": "valid_accuracy", "batch_size": 1000, "gnn_hidden_dim": hidden_dim,
+                                            "gnn_layer_num": gnn_layer_num, "num_heads": num_heads, "gnn_dropout_rate": dropout_rate,
+                                            "ffnn_hidden_dim": hidden_dim, "ffnn_layer_num": ffnn_layer_num, "ffnn_dropout_rate": dropout_rate
                                         })
                                 else:
                                     configurations.append({
                                         "benchmark": benchmark, "graph_type": graph_type, "model_type": model_type,
                                         "num_epochs": num_epochs,
                                         "learning_rate": 0.001,
-                                        "save_criterion": "valid_accuracy", "batch_size": 100, "gnn_hidden_dim": hidden_dim,
-                                        "gnn_layer_num": gnn_layer_num, "num_heads": 0, "gnn_dropout_rate": gnn_dropout_rate,
-                                        "ffnn_hidden_dim": hidden_dim, "ffnn_layer_num": ffnn_layer_num, "ffnn_dropout_rate": 0.5
+                                        "save_criterion": "valid_accuracy", "batch_size": 1000, "gnn_hidden_dim": hidden_dim,
+                                        "gnn_layer_num": gnn_layer_num, "num_heads": 0, "gnn_dropout_rate": dropout_rate,
+                                        "ffnn_hidden_dim": hidden_dim, "ffnn_layer_num": ffnn_layer_num, "ffnn_dropout_rate": dropout_rate
                                     })
 
     # Writing the dictionary to a JSON file
