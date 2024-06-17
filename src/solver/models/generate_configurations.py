7f462d03a1e3bc945f8bf03bf1f2eace7bb9bc58
Chencehng Liang <lcckkkhaha@163.com>
2024-06-17 14:38:06 +0200

    backup

--- src/solver/models/generate_configurations.py
+++ src/solver/models/generate_configurations.py
@@ -25,8 +25,10 @@ def main():
     node_type=5
     learning_rate=0.001
     train_step=10
+    label_size=2
     configurations = []
-    for benchmark in ["rank_01_track_multi_word_equations_generated_train_1_40000_new_divided_300_chunk_size_multiple_path"]: #["choose_eq_train"]:
+    #for benchmark in ["choose_eq_train"]:
+    for benchmark in  ["rank_01_track_multi_word_equations_generated_train_1_40000_new_divided_300_chunk_size_multiple_path"]:
         for graph_type in ["graph_1"]:#["graph_1","graph_2","graph_3","graph_4","graph_5"]:
             for gnn_layer_num in [2]:#[2,8]:
                 for ffnn_layer_num in [2]:
@@ -42,7 +44,7 @@ def main():
                                                     "num_epochs": num_epochs, "learning_rate": learning_rate,
                                                     "save_criterion": "valid_accuracy", "batch_size": batch_size, "gnn_hidden_dim": hidden_dim,
                                                     "gnn_layer_num": gnn_layer_num, "num_heads": num_heads, "gnn_dropout_rate": dropout_rate,
-                                                    "ffnn_hidden_dim": hidden_dim, "ffnn_layer_num": ffnn_layer_num,
+                                                    "ffnn_hidden_dim": hidden_dim, "ffnn_layer_num": ffnn_layer_num,"label_size":label_size,
                                                     "ffnn_dropout_rate": dropout_rate,"node_type":node_type,"train_step":train_step,"share_gnn":share_gnn
                                                 })
                                         else:
@@ -52,7 +54,7 @@ def main():
                                                 "learning_rate": learning_rate,
                                                 "save_criterion": "valid_accuracy", "batch_size": batch_size, "gnn_hidden_dim": hidden_dim,
                                                 "gnn_layer_num": gnn_layer_num, "num_heads": 0, "gnn_dropout_rate": dropout_rate,
-                                                "ffnn_hidden_dim": hidden_dim, "ffnn_layer_num": ffnn_layer_num,
+                                                "ffnn_hidden_dim": hidden_dim, "ffnn_layer_num": ffnn_layer_num,"label_size":label_size,
                                                 "ffnn_dropout_rate": dropout_rate,"node_type":node_type,"train_step":train_step,"share_gnn":share_gnn
                                             })
 
