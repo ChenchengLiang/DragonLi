c50421fa0f27bbb76562813fc566acd96477f259
Chencehng Liang <lcckkkhaha@163.com>
2025-01-31 02:04:09 +0100

    prepare train data

--- demo/script/generate_train_data_from_unsatcores.sh
+++ demo/script/generate_train_data_from_unsatcores.sh
@@ -4,6 +4,6 @@
 cd ../..
 
 
-apptainer exec --nv container/uppmax_word_equation_image.sif python3 src/train_data_collection/generate_train_data_from_unsatcores.py train_data
+apptainer exec --nv container/eval_image.sif python3 src/train_data_collection/generate_train_data_from_unsatcores.py train_data
 
 
--- demo/script/pickle_train_data.sh
+++ demo/script/pickle_train_data.sh
@@ -4,4 +4,6 @@
 cd ../..
 
 
-apptainer exec container/alvis_word_equation_recipe-A100.sif python3 src/train_data_collection/store_dataset_to_pickle_one_folder.py "graph_1" $2 $3 $4
+apptainer exec --nv container/eval_image.sif python3 src/train_data_collection/store_dataset_to_pickle_one_folder.py "graph_1" "train_data" "divided_1" "1"
+
+apptainer exec --nv container/eval_image.sif python3 src/train_data_collection/store_dataset_to_pickle_one_folder.py "graph_1" "train_data" "valid_data" "1"
